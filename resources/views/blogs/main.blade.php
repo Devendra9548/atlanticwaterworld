434d3a75a05190bc39f7e4942fc69b3e0c750af3
Devendra9548 <devendrasingh991731@gmail.com>
2025-03-10 15:09:22 +0530

    tablet version is finished

--- resources/views/blogs/main.blade.php
+++ resources/views/blogs/main.blade.php
@@ -179,7 +179,7 @@ $(document).ready(function() {
         responsive: [{
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 3,
                     infinite: true,
                     dots: true
